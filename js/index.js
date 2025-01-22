6a46be2c176c8c3ff41380ea20a7429184dba9a1
Abdurashidov-Xumoyun <okxumoyun@gmail.com>
2025-01-22 19:00:47 +0500

    JavaScriptda o'yin tugashi qo'shildi

--- js/index.js
+++ js/index.js
@@ -1,25 +1,72 @@
-
-
 let boxClick = document.querySelectorAll(".game_boxes");
 let newGame = document.querySelector(".btn");
 let innerIcon = document.querySelectorAll(".fa-solid");
+let InfoWinner = document.querySelector(".info_winner");
+let InfoBox = document.querySelector(".D_none-box");
+let InfoBoxLine = document.querySelector(".info_box");
 
-const xOrY = document.querySelector("#or");
+InfoBoxLine.addEventListener("click", () => {
+  InfoBox.style.display = "none";
+  for (let i = 0; i < innerIcon.length; i++) {
+    innerIcon[i].classList.remove("fa-x", "fa-o");
+  };
+});
 
-currentPlayer = "x";
 
-for (let i = 0; i < boxClick.length; i++) {
-  boxClick[i].addEventListener("click", boxClickMoment);
-  function boxClickMoment(e) {
-    let innerIcon = e.target.querySelector(".fa-solid");
+function orderNum() {
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let pattern of winPatterns) {
+    if (
+      innerIcon[pattern[0]].classList.contains("fa-x") &&
+      innerIcon[pattern[1]].classList.contains("fa-x") &&
+      innerIcon[pattern[2]].classList.contains("fa-x")
+    ) {
+      InfoBox.style.display = "block";
+      InfoWinner.innerHTML = `"X" mutloq g'olib`;
+    };
+
+    if (
+      innerIcon[pattern[0]].classList.contains("fa-o") &&
+      innerIcon[pattern[1]].classList.contains("fa-o") &&
+      innerIcon[pattern[2]].classList.contains("fa-o")
+    ) {
+      InfoBox.style.display = "block";
+      InfoWinner.innerHTML = `"O" mutloq g'olib`;
+    };
+
+  }
+};
+orderNum();
+
+
+function clickChange() {
+  const xOrY = document.querySelector("#or");
+  currentPlayer = "x";
+
+  for (let i = 0; i < boxClick.length; i++) {
+    boxClick[i].addEventListener("click", boxClickMoment);
+    function boxClickMoment(e) {
+      let innerIcon = e.target.querySelector(".fa-solid");
 
       if (!innerIcon.classList.contains("fa-x") && !innerIcon.classList.contains("fa-o")) {
         if (currentPlayer === "x") {
           innerIcon.classList.add("fa-x");
           currentPlayer = "o";
-        } else if (currentPlayer === "o"){
+          orderNum();
+        } else if (currentPlayer === "o") {
           innerIcon.classList.add("fa-o");
           currentPlayer = "x";
+          orderNum();
         }
       };
 
@@ -30,16 +77,19 @@ for (let i = 0; i < boxClick.length; i++) {
         xOrY.innerHTML = "O";
         xOrY.style.color = "green";
       };
-      
-  };
-};
 
-newGame.addEventListener("click", () => {
-  for (let i = 0; i < innerIcon.length; i++) {
-    innerIcon[i].classList.remove("fa-x");
-    innerIcon[i].classList.remove("fa-o"); 
+    };
   };
-});
+
+  newGame.addEventListener("click", () => {
+    for (let i = 0; i < innerIcon.length; i++) {
+      innerIcon[i].classList.remove("fa-x", "fa-o");
+    };
+  });
+
+}
+
+clickChange()
 
 
 
